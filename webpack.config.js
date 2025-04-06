5caf40dd4b843b9f70f069ba5eebc7ae2bc520da
Klimentin0 <meshkov.klim@gmail.com>
2025-04-06 22:00:03 +0300

    more modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const glob = require('glob');
+const fs = require('fs');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+}
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   return {
     mode: isProduction ? 'production' : 'development',
-    entry: './src/main.js',
+    entry: {
+     app: './src/main.js',
+     vendor: ['jquery']
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+      filename: '[name].[chunkhash].js', // placeholder name (dist/app.js), with hashing [], chunkhash -> unique hash.
       clean: true, // Cleans the dist folder before each build
       assetModuleFilename: 'images/[name][ext]', // Output directory for .webp files
     },
@@ -37,12 +48,53 @@ module.exports = (env, argv) => {
             },
           },
         },
+        {
+          test: /\.(webp|png|jpg|jpeg|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext]",
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+          type: "asset/resource", // Handles images as separate files
+          generator: {
+            filename: "images/[name][ext]", // Output directory and file name
+          },
+        },
       ],
     },
     plugins: [
       new MiniCssExtractPlugin({
         filename: isProduction ? '[name].[contenthash].css' : '[name].css', // Add hash for production
       }),
+      new PurgeCSSPlugin({
+        paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }), // PURGE EXTRA CSS
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["mozjpeg", { quality: 75 }],
+              ["pngquant", { quality: [0.65, 0.9] }],
+              ["svgo", { removeViewBox: false }],
+            ],
+          },
+        },
+      }),
+// MANIFEST
+      {
+        apply(compiler) {
+          compiler.hooks.done.tap('GenerateManifestPlugin', (stats) => {
+            fs.writeFileSync(
+              path.join(__dirname, 'dist/manifest.json'),
+              JSON.stringify(stats.toJson())
+            );
+          });
+        },
+      },
     ],
     devtool: isProduction ? false : 'eval-source-map', // Disable source maps in production
   };
